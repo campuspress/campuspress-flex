8f3b7145047a493a6e41986fe6ab4762668c8d6e
Mariusz Misiek <maniu@incsub.com>
2020-07-16 08:49:34 +0200

    Moved "block_editor_settings" filter to editor.php

--- inc/hooks-wp.php
+++ inc/hooks-wp.php
@@ -141,16 +141,6 @@ if ( ! function_exists( 'cpschool_adjacent_post_link_change' ) ) {
 	}
 }
 
-if ( ! function_exists( 'cpschool_block_editor_settings' ) ) {
-	add_filter( 'block_editor_settings', 'cpschool_block_editor_settings', 10, 2 );
-
-	function cpschool_block_editor_settings( $editor_settings, $post ) {
-		$editor_settings['styles'][] = array( 'css' => 'body { font-family: "Inter var"; }' );
-
-		return $editor_settings;
-	}
-}
-
 if ( ! function_exists( 'cpschool_show_reusable_blocks_admin' ) ) {
 	add_filter( 'register_post_type_args', 'cpschool_show_reusable_blocks_admin', 10, 2 );
 
