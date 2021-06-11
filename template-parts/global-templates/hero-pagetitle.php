5a7999cf96555d74b60a2400334b6a11713ff70d
Suman Basuli <thinisadhu@gmail.com>
2021-06-11 10:01:46 +0530

    Added function for displaying image

--- template-parts/global-templates/hero-pagetitle.php
+++ template-parts/global-templates/hero-pagetitle.php
@@ -1,6 +1,42 @@
 <?php
 $hero_style = cpschool_get_hero_style();
 
+function img_placement( $img_position, $img_class ){
+	global $hero_style;
+
+	$thumbnail_post_id = false;
+	if ( is_singular() ) {
+		$thumbnail_post_id = get_the_ID();
+	} elseif ( is_home() ) {
+		$thumbnail_post_id = get_option( 'page_for_posts' );
+	}
+	$thumbnail_size = 'hero';
+	if ( $hero_style == $img_position && ! is_customize_preview() ) {
+		$thumbnail_size = 'large';
+	}
+	if ( is_singular() && has_post_thumbnail( $thumbnail_post_id ) ) {
+		if ( $img_class == 'hero-image-top' ){
+			?>
+			<div class="hero-image-holder hero-featured-image-holder hero-image-top" data-aos="fade" data-aos-duration="1000">
+			<?php
+		} else { ?>
+			<div class="hero-image-holder hero-featured-image-holder hero-image-bottom" data-aos="fade" data-aos-duration="1000">
+			<?php
+		}
+				echo get_the_post_thumbnail( $thumbnail_post_id, $thumbnail_size ); ?>
+			</div>
+		<?php
+	} elseif ( ( $hero_style != $img_position || is_customize_preview() ) && $hero_default_images = get_theme_mod( 'hero_main_default_images' ) ) {
+		$thumbnail_id = $hero_default_images[ mt_rand( 0, count( $hero_default_images ) - 1 ) ]['id'];
+		?>
+	
+		<div <?php cpschool_class( 'hero-main-default-image-holder', 'hero-image-holder hero-default-image-holder' ); ?> data-aos="fade" data-aos-duration="1000">
+			<?php echo wp_get_attachment_image( $thumbnail_id, $thumbnail_size ); ?>
+		</div>
+		<?php
+	}
+}
+
 if ( $hero_style || ( is_customize_preview() && ( ! is_singular() || ! get_post_meta( get_the_ID(), 'cps_hero_title_disable', true ) ) ) ) {
 	$title = cpschool_get_page_title();
 	?>
@@ -8,31 +44,7 @@ if ( $hero_style || ( is_customize_preview() && ( ! is_singular() || ! get_post_
 		
 		<?php
 			if ($hero_style == 'img-above-title' || is_customize_preview()){
-				$thumbnail_post_id = false;
-				if ( is_singular() ) {
-					$thumbnail_post_id = get_the_ID();
-				} elseif ( is_home() ) {
-					$thumbnail_post_id = get_option( 'page_for_posts' );
-				}
-				$thumbnail_size = 'hero';
-				if ( $hero_style == 'img-above-title' && ! is_customize_preview() ) {
-					$thumbnail_size = 'large';
-				}
-				if ( is_singular() && has_post_thumbnail( $thumbnail_post_id ) ) {
-					?>
-					<div class="hero-image-holder hero-featured-image-holder hero-image-top" data-aos="fade" data-aos-duration="1000">
-						<?php echo get_the_post_thumbnail( $thumbnail_post_id, $thumbnail_size ); ?>
-					</div>
-					<?php
-				} elseif ( ( $hero_style != 'img-above-title' || is_customize_preview() ) && $hero_default_images = get_theme_mod( 'hero_main_default_images' ) ) {
-					$thumbnail_id = $hero_default_images[ mt_rand( 0, count( $hero_default_images ) - 1 ) ]['id'];
-					?>
-				
-					<div <?php cpschool_class( 'hero-main-default-image-holder', 'hero-image-holder hero-default-image-holder' ); ?> data-aos="fade" data-aos-duration="1000">
-						<?php echo wp_get_attachment_image( $thumbnail_id, $thumbnail_size ); ?>
-					</div>
-					<?php
-				}
+				img_placement( 'img-above-title', 'hero-image-top' );
 			}
 			?>
 
@@ -75,31 +87,7 @@ if ( $hero_style || ( is_customize_preview() && ( ! is_singular() || ! get_post_
 		
 		<?php
 		if ($hero_style != 'img-above-title' || is_customize_preview()){
-			$thumbnail_post_id = false;
-			if ( is_singular() ) {
-				$thumbnail_post_id = get_the_ID();
-			} elseif ( is_home() ) {
-				$thumbnail_post_id = get_option( 'page_for_posts' );
-			}
-			$thumbnail_size = 'hero';
-			if ( $hero_style == 'img-under-title' && ! is_customize_preview() ) {
-				$thumbnail_size = 'large';
-			}
-			if ( is_singular() && has_post_thumbnail( $thumbnail_post_id ) ) {
-				?>
-				<div class="hero-image-holder hero-featured-image-holder hero-image-bottom" data-aos="fade" data-aos-duration="1000">
-					<?php echo get_the_post_thumbnail( $thumbnail_post_id, $thumbnail_size ); ?>
-				</div>
-				<?php
-			} elseif ( ( $hero_style != 'img-under-title' || is_customize_preview() ) && $hero_default_images = get_theme_mod( 'hero_main_default_images' ) ) {
-				$thumbnail_id = $hero_default_images[ mt_rand( 0, count( $hero_default_images ) - 1 ) ]['id'];
-				?>
-			
-				<div <?php cpschool_class( 'hero-main-default-image-holder', 'hero-image-holder hero-default-image-holder' ); ?> data-aos="fade" data-aos-duration="1000">
-					<?php echo wp_get_attachment_image( $thumbnail_id, $thumbnail_size ); ?>
-				</div>
-				<?php
-			}
+			img_placement( 'img-under-title', 'hero-image-bottom' );
 		}
 		?>
 		 
