515312e1dfe0e03c486d2bf414889eff2cf6dbef
Mariusz Misiek <maniu@incsub.com>
2021-07-07 11:57:24 +0200

    search form bugfixing

--- searchform.php
+++ searchform.php
@@ -16,7 +16,7 @@ if ( ! isset( $args['id'] ) || ! $args['id'] ) {
 <form method="get" id="<?php echo esc_attr( $args['id'] ); ?>" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<label class="sr-only" for="<?php echo esc_attr( $args['id'] . '-s' ); ?>"><?php esc_html_e( 'Search', 'cpschool' ); ?></label>
 	<div class="input-group">
-		<input class="field form-control" id="<?php echo esc_attr( $args['id'] . '-s' ); ?>" name="<?php echo esc_attr( $args['id'] . '-s' ); ?>" type="text" placeholder="<?php esc_attr_e( 'Search...', 'cpschool' ); ?>" value="<?php the_search_query(); ?>">
+		<input class="field form-control" id="<?php echo esc_attr( $args['id'] . '-s' ); ?>" name="s" type="text" placeholder="<?php esc_attr_e( 'Search...', 'cpschool' ); ?>" value="<?php the_search_query(); ?>">
 		<span class="input-group-append">
 			<button class="submit btn btn-primary" id="<?php echo esc_attr( $args['id'] . '-submit' ); ?>" name="submit" type="submit">
 				<i aria-hidden="true" class="cps-icon cps-icon-search"></i>
