41ec41134b7a18a3a31ce9a965442cd68f3ded3e
Andrey Islentiev <andrey.islentiev@incsub.com>
2022-10-21 14:39:21 +1000

    fix: parent link of collapsing menu in mobile nav doesn't load new page

--- js/theme.js
+++ js/theme.js
@@ -7468,8 +7468,20 @@ var cpSchoolThemeHelpers = function ($) {
         }
       }
     });
-  } // This will make vars in css work in IE11.
+  }
 
+  $('a.collapse-toggle').on('touchend', function (e) {
+    var isExpanded = $(this).attr('aria-expanded');
+
+    if (isExpanded === 'true') {
+      e.preventDefault();
+      var link = $(this).attr('href');
+
+      if (!link.startsWith('#')) {
+        window.location = link;
+      }
+    }
+  }); // This will make vars in css work in IE11.
 
   if (typeof cssVars === "function") {
     cssVars({
