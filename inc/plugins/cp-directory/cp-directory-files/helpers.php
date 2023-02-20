e20210ffd524918629a92dc28c8c1108f4e724c5
Mariusz Misiek <maniu@incsub.com>
2023-02-20 12:48:59 +0100

    fixed possible custom field related warnings in directory

--- inc/plugins/cp-directory/cp-directory-files/helpers.php
+++ inc/plugins/cp-directory/cp-directory-files/helpers.php
@@ -184,64 +184,67 @@ function cp_dir_get_field_value( $entry_id, $field_details ) {
 	}
 
 	$value = array();
-	switch ( $field_details['type'] ) :
-		case 'post':
-			switch ( $field_details['name'] ) :
-				case 'post_title':
-					$entry_post = get_post( $entry_id );
-					$value_raw_content = get_the_title( $entry_post );
-					$value_raw_attr    = $value_raw_content;
-					if ( isset( $field_details['args']['link'] ) && $field_details['args']['link'] ) {
-						$link = get_permalink( $entry_post );
-						if( apply_filters( 'cp_dir_enable_go_back', true ) ) {
-							if ( get_the_ID() ) {
-								$link = add_query_arg( 'cp-dir-id', get_the_ID(), $link );
+
+	if( isset( $field_details['type'] ) ) {
+		switch ( $field_details['type'] ) :
+			case 'post':
+				switch ( $field_details['name'] ) :
+					case 'post_title':
+						$entry_post = get_post( $entry_id );
+						$value_raw_content = get_the_title( $entry_post );
+						$value_raw_attr    = $value_raw_content;
+						if ( isset( $field_details['args']['link'] ) && $field_details['args']['link'] ) {
+							$link = get_permalink( $entry_post );
+							if( apply_filters( 'cp_dir_enable_go_back', true ) ) {
+								if ( get_the_ID() ) {
+									$link = add_query_arg( 'cp-dir-id', get_the_ID(), $link );
+								}
 							}
+							$value_raw_content = '<a href="' . esc_url( $link ) . '">' . $value_raw_content . '</a>';
 						}
-						$value_raw_content = '<a href="' . esc_url( $link ) . '">' . $value_raw_content . '</a>';
-					}
-					break;
-				case 'post_excerpt':
-					$entry_post = get_post( $entry_id );
-					$value_raw_content = wp_trim_excerpt( $entry_post->post_excerpt, $entry_post );
-					break;
-			endswitch;
-			break;
-		case 'custom_field':
-			$value_raw_content = get_post_meta( $entry_id, $field_details['name'], true );
-			break;
-		case 'taxonomy':
-			// For some reason "fields => 'id=>name'" is not working here.
-			$args = array( 'hierarchical' => true );
-			if ( isset( $field_details['args']['parent_id'] ) ) {
-				// Includes all children and parent so it works when parent is not selected in entry.
-				$term_include    = get_term_children( $field_details['args']['parent_id'], $field_details['name'] );
-				//looks like this is no longer needed?
-				//$term_include[]  = $field_details['args']['parent_id'];
-				$args['include'] = $term_include;
-			}
-			$entry_taxonomies       = wp_get_post_terms( $entry_id, $field_details['name'], $args );
-			$entry_taxonomies_names = $entry_taxonomies_ids = array();
-			foreach ( $entry_taxonomies as $entry_taxonomy ) {
-				$entry_taxonomies_names[] = $entry_taxonomy->name;
-				$entry_taxonomies_ids[]   = $entry_taxonomy->term_id;
-				if ( $entry_taxonomy->parent && !in_array($entry_taxonomy->parent, $entry_taxonomies_ids ) ) {
-					$entry_taxonomies_ids[] = $entry_taxonomy->parent;
+						break;
+					case 'post_excerpt':
+						$entry_post = get_post( $entry_id );
+						$value_raw_content = wp_trim_excerpt( $entry_post->post_excerpt, $entry_post );
+						break;
+				endswitch;
+				break;
+			case 'custom_field':
+				$value_raw_content = get_post_meta( $entry_id, $field_details['name'], true );
+				break;
+			case 'taxonomy':
+				// For some reason "fields => 'id=>name'" is not working here.
+				$args = array( 'hierarchical' => true );
+				if ( isset( $field_details['args']['parent_id'] ) ) {
+					// Includes all children and parent so it works when parent is not selected in entry.
+					$term_include    = get_term_children( $field_details['args']['parent_id'], $field_details['name'] );
+					//looks like this is no longer needed?
+					//$term_include[]  = $field_details['args']['parent_id'];
+					$args['include'] = $term_include;
 				}
-			}
+				$entry_taxonomies       = wp_get_post_terms( $entry_id, $field_details['name'], $args );
+				$entry_taxonomies_names = $entry_taxonomies_ids = array();
+				foreach ( $entry_taxonomies as $entry_taxonomy ) {
+					$entry_taxonomies_names[] = $entry_taxonomy->name;
+					$entry_taxonomies_ids[]   = $entry_taxonomy->term_id;
+					if ( $entry_taxonomy->parent && !in_array($entry_taxonomy->parent, $entry_taxonomies_ids ) ) {
+						$entry_taxonomies_ids[] = $entry_taxonomy->parent;
+					}
+				}
 
-			// If entry is checking in children and finds some, lets say it is also part of parent.
-			if ( isset( $field_details['args']['parent_id'] ) && $entry_taxonomies_ids ) {
-				$entry_taxonomies_ids[] = $field_details['args']['parent_id'];
-			}
+				// If entry is checking in children and finds some, lets say it is also part of parent.
+				if ( isset( $field_details['args']['parent_id'] ) && $entry_taxonomies_ids ) {
+					$entry_taxonomies_ids[] = $field_details['args']['parent_id'];
+				}
 
-			//looks like this is no longer needed?
-			//$entry_taxonomies_ids = array_unique( $entry_taxonomies_ids );
+				//looks like this is no longer needed?
+				//$entry_taxonomies_ids = array_unique( $entry_taxonomies_ids );
 
-			$value_raw_content = implode( ', ', $entry_taxonomies_names );
-			$value_raw_attr    = implode( ',', $entry_taxonomies_ids );
-			break;
-	endswitch;
+				$value_raw_content = implode( ', ', $entry_taxonomies_names );
+				$value_raw_attr    = implode( ',', $entry_taxonomies_ids );
+				break;
+		endswitch;
+	}
 
 	if ( ! isset( $value_raw_attr ) ) {
 		$value_raw_attr = false;
