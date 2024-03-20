fbadedc2486cd0354fd33bf27763e7de377bfc47
Mariusz Misiek <maniu@incsub.com>
2024-03-20 09:02:35 +0100

    name="submit" removed from search form

--- searchform.php
+++ searchform.php
@@ -24,7 +24,7 @@ if ( empty( $args['btn_class'] ) ) {
 	<div class="input-group">
 		<input class="<?php echo esc_attr( $args['field_class'] ); ?>" id="<?php echo esc_attr( $args['id'] . '-s' ); ?>" name="s" type="text" placeholder="<?php esc_attr_e( 'Search...', 'cpschool' ); ?>" value="<?php the_search_query(); ?>">
 		<span class="input-group-append">
-			<button class="<?php echo esc_attr( $args['btn_class'] ); ?>" id="<?php echo esc_attr( $args['id'] . '-submit' ); ?>" name="submit" type="submit">
+			<button class="<?php echo esc_attr( $args['btn_class'] ); ?>" id="<?php echo esc_attr( $args['id'] . '-submit' ); ?>" type="submit">
 				<i aria-hidden="true" class="cps-icon cps-icon-search"></i>
 				<span class="sr-only"><?php _e( 'Search Site', 'cpschool' ); ?></span>
 			</button>
